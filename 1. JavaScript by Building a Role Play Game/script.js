df92c4542ef47592633fda5ab2faecd9e077e6a1
Muhammad Imran <imran5244513@gmail.com>
2024-02-05 10:51:17 +0500

    JavaScript is a powerful scripting language that you can use to make web pages interactive. It's one of the core technologies of the web, along with HTML and CSS. All modern browsers support JavaScript.
    In this practice project, you'll learn fundamental programming concepts in JavaScript by coding your own Role Playing Game. You'll learn how to work with arrays, strings, objects, functions, loops, if/else statements, and more.

--- 1. JavaScript by Building a Role Play Game/script.js
+++ 1. JavaScript by Building a Role Play Game/script.js
@@ -16,6 +16,12 @@ const goldText = document.querySelector('#goldText');
 const monsterStats = document.querySelector('#monsterStats');
 const monsterName = document.querySelector('#monsterName');
 const monsterHealthText = document.querySelector('#monsterHealth');
+const weapons = [
+  { name: 'stick', power: 5 },
+  { name: 'dagger', power: 30 },
+  { name: 'claw hammer', power: 50 },
+  { name: 'sword', power: 100 },
+];
 const locations = [
   {
     name: 'town square',
@@ -73,11 +79,31 @@ function fightDragon() {
 }
 
 function buyHealth() {
-  gold -= 10;
-  health += 10;
+  if (gold >= 10) {
+    gold -= 10;
+    health += 10;
+    goldText.innerText = gold;
+    healthText.innerText = health;
+  } else {
+    text.innerText = 'You do not have enough gold to buy health.';
+  }
 }
 
-function buyWeapon() {}
+function buyWeapon() {
+  if (currentWeapon < 3) {
+    if (gold >= 30) {
+      gold -= 30;
+      currentWeapon++;
+      goldText.innerText = gold;
+      let newWeapon = weapons[currentWeapon].name;
+      text.innerText = 'You now have a ' + newWeapon + '.';
+      inventory.push(newWeapon);
+      text.innerText += ' In your inventory you have: ' + inventory;
+    } else {
+      text.innerText = 'You do not have enough gold to buy a weapon.';
+    }
+  }
+}
 
 function fightSlime() {}
 
