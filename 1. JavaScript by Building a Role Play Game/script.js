eff3c0eeb5209ea386c231bc2c88cb3fbbd609aa
Muhammad Imran <imran5244513@gmail.com>
2024-02-06 09:36:46 +0500

    JavaScript is a powerful scripting language that you can use to make web pages interactive. It's one of the core technologies of the web, along with HTML and CSS. All modern browsers support JavaScript.
    In this practice project, you'll learn fundamental programming concepts in JavaScript by coding your own Role Playing Game. You'll learn how to work with arrays, strings, objects, functions, loops, if/else statements, and more.

--- 1. JavaScript by Building a Role Play Game/script.js
+++ 1. JavaScript by Building a Role Play Game/script.js
@@ -75,7 +75,7 @@ const locations = [
       'Go to town square',
       'Go to town square',
     ],
-    'button functions': [goTown, goTown, goTown],
+    'button functions': [goTown, goTown, easterEgg],
     text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.',
   },
   {
@@ -89,6 +89,12 @@ const locations = [
     'button text': ['REPLAY?', 'REPLAY?', 'REPLAY?'],
     'button functions': [restart, restart, restart],
     text: 'You defeat the dragon! YOU WIN THE GAME! 🎉',
+  },
+  {
+    name: 'easter egg',
+    'button text': ['2', '8', 'Go to town square?'],
+    'button functions': [pickTwo, pickEight, goTown],
+    text: 'You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!',
   },
 ];
 
@@ -258,4 +264,35 @@ function restart() {
 
 function easterEgg() {
   update(locations[7]);
+}
+
+function pickTwo() {
+  pick(2);
+}
+
+function pickEight() {
+  pick(8);
+}
+
+function pick(guess) {
+  const numbers = [];
+  while (numbers.length < 10) {
+    numbers.push(Math.floor(Math.random() * 11));
+  }
+  text.innerText = 'You picked ' + guess + '. Here are the random numbers:\n';
+  for (let i = 0; i < 10; i++) {
+    text.innerText += numbers[i] + '\n';
+  }
+  if (numbers.includes(guess)) {
+    text.innerText += 'Right! You win 20 gold!';
+    gold += 20;
+    goldText.innerText = gold;
+  } else {
+    text.innerText += 'Wrong! You lose 10 health!';
+    health -= 10;
+    healthText.innerText = health;
+    if (health <= 0) {
+      lose();
+    }
+  }
 }
