4a799bf68e06cb5bb836f63414e9ab9d7e6de31b
Muhammad Imran <imran5244513@gmail.com>
2024-02-06 09:08:10 +0500

    JavaScript is a powerful scripting language that you can use to make web pages interactive. It's one of the core technologies of the web, along with HTML and CSS. All modern browsers support JavaScript.
    In this practice project, you'll learn fundamental programming concepts in JavaScript by coding your own Role Playing Game. You'll learn how to work with arrays, strings, objects, functions, loops, if/else statements, and more.

--- 1. JavaScript by Building a Role Play Game/script.js
+++ 1. JavaScript by Building a Role Play Game/script.js
@@ -68,6 +68,28 @@ const locations = [
     'button functions': [attack, dodge, goTown],
     text: 'You are fighting a monster.',
   },
+  {
+    name: 'kill monster',
+    'button text': [
+      'Go to town square',
+      'Go to town square',
+      'Go to town square',
+    ],
+    'button functions': [goTown, goTown, goTown],
+    text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.',
+  },
+  {
+    name: 'lose',
+    'button text': ['REPLAY?', 'REPLAY?', 'REPLAY?'],
+    'button functions': [restart, restart, restart],
+    text: 'You die. ☠️',
+  },
+  {
+    name: 'win',
+    'button text': ['REPLAY?', 'REPLAY?', 'REPLAY?'],
+    'button functions': [restart, restart, restart],
+    text: 'You defeat the dragon! YOU WIN THE GAME! 🎉',
+  },
 ];
 
 // initialize buttons
@@ -76,6 +98,7 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function update(location) {
+  monsterStats.style.display = 'none';
   button1.innerText = location['button text'][0];
   button2.innerText = location['button text'][1];
   button3.innerText = location['button text'][2];
@@ -157,8 +180,82 @@ function fightDragon() {
 
 function goFight() {
   update(locations[3]);
+  monsterHealth = monsters[fighting].health;
+  monsterStats.style.display = 'block';
+  monsterName.innerText = monsters[fighting].name;
+  monsterHealthText.innerText = monsterHealth;
 }
 
-function attack() {}
+function attack() {
+  text.innerText = 'The ' + monsters[fighting].name + ' attacks.';
+  text.innerText +=
+    ' You attack it with your ' + weapons[currentWeapon].name + '.';
+  health -= getMonsterAttackValue(monsters[fighting].level);
+  if (isMonsterHit()) {
+    monsterHealth -=
+      weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
+  } else {
+    text.innerText += ' You miss.';
+  }
+  healthText.innerText = health;
+  monsterHealthText.innerText = monsterHealth;
+  if (health <= 0) {
+    lose();
+  } else if (monsterHealth <= 0) {
+    if (fighting === 2) {
+      winGame();
+    } else {
+      defeatMonster();
+    }
+  }
+  if (Math.random() <= 0.1 && inventory.length !== 1) {
+    text.innerText += ' Your ' + inventory.pop() + ' breaks.';
+    currentWeapon--;
+  }
+}
 
-function dodge() {}
+function getMonsterAttackValue(level) {
+  const hit = level * 5 - Math.floor(Math.random() * xp);
+  console.log(hit);
+  return hit > 0 ? hit : 0;
+}
+
+function isMonsterHit() {
+  return Math.random() > 0.2 || health < 20;
+}
+
+function dodge() {
+  text.innerText = 'You dodge the attack from the ' + monsters[fighting].name;
+}
+
+function defeatMonster() {
+  gold += Math.floor(monsters[fighting].level * 6.7);
+  xp += monsters[fighting].level;
+  goldText.innerText = gold;
+  xpText.innerText = xp;
+  update(locations[4]);
+}
+
+function lose() {
+  update(locations[5]);
+}
+
+function winGame() {
+  update(locations[6]);
+}
+
+function restart() {
+  xp = 0;
+  health = 100;
+  gold = 50;
+  currentWeapon = 0;
+  inventory = ['stick'];
+  goldText.innerText = gold;
+  healthText.innerText = health;
+  xpText.innerText = xp;
+  goTown();
+}
+
+function easterEgg() {
+  update(locations[7]);
+}
