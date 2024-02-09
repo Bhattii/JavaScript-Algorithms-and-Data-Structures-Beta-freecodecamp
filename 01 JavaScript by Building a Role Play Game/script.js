eb22d2258620f8e174447af2f73e3c39cad561be
Muhammad Imran <imran5244513@gmail.com>
2024-02-09 19:55:43 +0500

    JavaScript is a powerful scripting language that you can use to make web pages interactive. It's one of the core technologies of the web, along with HTML and CSS. All modern browsers support JavaScript.
    In this practice project, you'll learn fundamental programming concepts in JavaScript by coding your own Role Playing Game. You'll learn how to work with arrays, strings, objects, functions, loops, if/else statements, and more.

--- 01 JavaScript by Building a Role Play Game/script.js
+++ 01 JavaScript by Building a Role Play Game/script.js
@@ -111,7 +111,7 @@ function update(location) {
   button1.onclick = location['button functions'][0];
   button2.onclick = location['button functions'][1];
   button3.onclick = location['button functions'][2];
-  text.innerText = location.text;
+  text.innerHTML = location.text;
 }
 
 function goTown() {
