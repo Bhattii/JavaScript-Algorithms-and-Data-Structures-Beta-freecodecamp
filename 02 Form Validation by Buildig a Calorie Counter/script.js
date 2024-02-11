cafd2b4c3c62175224656508318489513e480db5
Muhammad Imran <imran5244513@gmail.com>
2024-02-11 20:34:19 +0500

    Sometimes when you're coding a web application, you'll need to be able to accept input from a user. In this calorie counter project, you'll learn how to validate user input, perform calculations based on that input, and dynamically update your interface to display the results.
    In this practice project, you'll learn basic regular expressions, template literals, the addEventListener() method, and more.

--- 02 Form Validation by Buildig a Calorie Counter/script.js
+++ 02 Form Validation by Buildig a Calorie Counter/script.js
@@ -10,3 +10,27 @@ function cleanInputString(str) {
   const regex = /[+-\s]/g;
   return str.replace(regex, '');
 }
+
+function isInvalidInput(str) {
+  const regex = /\d+e\d+/i;
+  return str.match(regex);
+}
+
+function addEntry() {
+  const targetInputContainer = document.querySelector(
+    `#${entryDropdown.value} .input-container`
+  );
+  const entryNumber =
+    targetInputContainer.querySelectorAll('input[type="text"]').length;
+  const HTMLString = `
+  <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+  <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
+  <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+  <input
+    type="number"
+    min="0"
+    id="${entryDropdown.value}-${entryNumber}-calories"
+    placeholder="Calories"
+  />`;
+  targetInputContainer.innerHTML += HTMLString;
+}
