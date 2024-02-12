5f087085bb159dc4e413872669e78937cd1a7c5b
Muhammad Imran <imran5244513@gmail.com>
2024-02-12 19:11:20 +0500

    Sometimes when you're coding a web application, you'll need to be able to accept input from a user. In this calorie counter project, you'll learn how to validate user input, perform calculations based on that input, and dynamically update your interface to display the results.
    In this practice project, you'll learn basic regular expressions, template literals, the addEventListener() method, and more.

--- 02 Form Validation by Buildig a Calorie Counter/script.js
+++ 02 Form Validation by Buildig a Calorie Counter/script.js
@@ -68,6 +68,20 @@ function calculateCalories(e) {
 
   const consumedCalories =
     breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
+  const remainingCalories =
+    budgetCalories - consumedCalories + exerciseCalories;
+  const surplusOrDeficit = remainingCalories < 0 ? 'Surplus' : 'Deficit';
+  output.innerHTML = `
+  <span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(
+    remainingCalories
+  )} Calorie ${surplusOrDeficit}</span>
+  <hr>
+  <p>${budgetCalories} Calories Budgeted</p>
+  <p>${consumedCalories} Calories Consumed</p>
+  <p>${exerciseCalories} Calories Burned</p>
+  `;
+
+  output.classList.remove('hide');
 }
 
 function getCaloriesFromInputs(list) {
@@ -87,4 +101,20 @@ function getCaloriesFromInputs(list) {
   return calories;
 }
 
+function clearForm() {
+  const inputContainers = Array.from(
+    document.querySelectorAll('.input-container')
+  );
+
+  for (const container of inputContainers) {
+    container.innerHTML = '';
+  }
+
+  budgetNumberInput.value = '';
+  output.innerText = '';
+  output.classList.add('hide');
+}
+
 addEntryButton.addEventListener('click', addEntry);
+calorieCounter.addEventListener('submit', calculateCalories);
+clearButton.addEventListener('click', clearForm);
