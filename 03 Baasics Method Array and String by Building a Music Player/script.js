73caade5cf17ca5695c16e7fdb4f46ab8ae17d3f
Muhammad Imran <imran5244513@gmail.com>
2024-02-18 09:27:39 +0500

    Hightlight the Current Playing Song from the song list

--- 03 Baasics Method Array and String by Building a Music Player/script.js
+++ 03 Baasics Method Array and String by Building a Music Player/script.js
@@ -98,6 +98,7 @@ const playSong = (id) => {
   userData.currentSong = song;
   playButton.classList.add('playing');
 
+  highlightCurrentSong();
   audio.play();
 };
 
@@ -129,11 +130,19 @@ const playPreviousSong = () => {
   }
 };
 
+const setPlayerDisplay = () => {};
+
 const highlightCurrentSong = () => {
   const playlistSongElements = document.querySelectorAll('.playlist-song');
   const songToHighlight = document.getElementById(
     `song-${userData?.currentSong?.id}`
   );
+
+  playlistSongElements.forEach((songEl) => {
+    songEl.removeAttribute('aria-current');
+  });
+
+  if (songToHighlight) songToHighlight.setAttribute('aria-current', 'true');
 };
 
 const renderSongs = (array) => {
