60a82a7738fb381c513e8fd0dc5563b998f8d697
Muhammad Imran <imran5244513@gmail.com>
2024-02-19 17:33:18 +0500

    updating

---
+++ 05 Palindmor Checkup Project/scripta.js
@@ -0,0 +1,12 @@
+const userInput = document.getElementById('text-input');
+const checkPalindromeBtn = document.getElementById('check-btn');
+const resultDiv = document.getElementById('result');
+
+const checkForPalindrome = (input) => {
+  const orginalInput = input;
+};
+if (input === '') {
+  alert('please input a value');
+  return;
+}
+resultDiv.replaceChildren();
