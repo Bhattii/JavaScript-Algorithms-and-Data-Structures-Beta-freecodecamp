ec060877dd4f5d92d00f962961c8527995f40664
Muhammad Imran <imran5244513@gmail.com>
2024-02-21 16:20:21 +0500

    One common aspect of building web applications: processing datasets, and then outputting information to the screen. In this sports team cards project, you'll learn how to work with DOM manipulation, object destructuring, event handling, and data filtering.
    This project will cover concepts like switch statements, default parameters, Object.freeze(), the map() method, and more.

--- 06 Modern JavaScript Methods by Building Football Team Cards/script.js
+++ 06 Modern JavaScript Methods by Building Football Team Cards/script.js
@@ -181,14 +181,48 @@ worldCupYear.textContent = year;
 headCoach.textContent = coachName;
 
 const setPlayerCards = (arr = players) => {
-  playerCards.innerHTML += arr.map(
-    ({ name, position, number, isCaptain, nickname }) => {
-      `
+  playerCards.innerHTML += arr
+    .map(
+      ({ name, position, number, isCaptain, nickname }) =>
+        `
         <div class="player-card">
           <h2>${name} ${isCaptain ? '(Captain)' : ''}</h2>
-          
+          <p>Position: ${position}</p>
+          <p>Number: ${number}</p>
+          <p>Nickname: ${nickname !== null ? nickname : 'N/A'}</p>
         </div>
-      `;
-    }
-  );
+      `
+    )
+    .join('');
 };
+
+playersDropdownList.addEventListener('change', (e) => {
+  playerCards.innerHTML = '';
+
+  switch (e.target.value) {
+    case 'nickname':
+      setPlayerCards(players.filter((player) => player.nickname !== null));
+      break;
+    case 'forward':
+      setPlayerCards(players.filter((player) => player.position === 'forward'));
+      break;
+    case 'midfielder':
+      setPlayerCards(
+        players.filter((player) => player.position === 'midfielder')
+      );
+      break;
+    case 'defender':
+      setPlayerCards(
+        players.filter((player) => player.position === 'defender')
+      );
+      break;
+    case 'goalkeeper':
+      setPlayerCards(
+        players.filter((player) => player.position === 'goalkeeper')
+      );
+      break;
+
+    default:
+      setPlayerCards();
+  }
+});
