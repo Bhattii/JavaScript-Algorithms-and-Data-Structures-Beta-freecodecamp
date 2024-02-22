b3d072028be3e0cdf740ef8e0c12e7b672adffe3
Muhammad Imran <imran5244513@gmail.com>
2024-02-22 20:07:51 +0500

    update

--- 07 Local Storage by Building a Todo App/script.js
+++ 07 Local Storage by Building a Todo App/script.js
@@ -4,3 +4,38 @@ const openTaskFormBtn = document.getElementById('open-task-form-btn');
 const closeTaskFormBtn = document.getElementById('close-task-form-btn');
 const addOrUpdateTaskBtn = document.getElementById('add-or-update-task-btn');
 const cancelBtn = document.getElementById('cancel-btn');
+const discardBtn = document.getElementById('discard-btn');
+const tasksContainer = document.getElementById('tasks-container');
+const titleInput = document.getElementById('title-input');
+const dateInput = document.getElementById('date-input');
+const descriptionInput = document.getElementById('description-input');
+
+const taskData = [];
+let currentTask = {};
+
+openTaskFormBtn.addEventListener('click', () =>
+  taskForm.classList.toggle('hidden')
+);
+
+closeTaskFormBtn.addEventListener('click', () => {
+  confirmCloseDialog.showModal();
+});
+
+cancelBtn.addEventListener('click', () => confirmCloseDialog.close());
+
+discardBtn.addEventListener('click', () => {
+  confirmCloseDialog.close();
+  taskForm.classList.toggle('hidden');
+});
+
+taskForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+  const taskObj = {
+    id: `${titleInput.value.toLowerCase().split(' ').join('-')}-${Date.now()}`,
+    title: titleInput.value,
+    date: dateInput.value,
+    description: descriptionInput.value,
+  };
+});
