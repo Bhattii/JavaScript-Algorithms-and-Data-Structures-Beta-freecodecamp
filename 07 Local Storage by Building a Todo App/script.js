abb6163ade32f285445723149aac21a3257cb438
Muhammad Imran <imran5244513@gmail.com>
2024-02-22 20:06:17 +0500

    adding js file

---
+++ 07 Local Storage by Building a Todo App/script.js
@@ -0,0 +1,6 @@
+const taskForm = document.getElementById('task-form');
+const confirmCloseDialog = document.getElementById('confirm-close-dialog');
+const openTaskFormBtn = document.getElementById('open-task-form-btn');
+const closeTaskFormBtn = document.getElementById('close-task-form-btn');
+const addOrUpdateTaskBtn = document.getElementById('add-or-update-task-btn');
+const cancelBtn = document.getElementById('cancel-btn');
