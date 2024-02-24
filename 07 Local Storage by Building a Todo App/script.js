3d930df7de28416bf0930185f3dc62ba63c84c66
Muhammad Imran <imran5244513@gmail.com>
2024-02-24 20:53:18 +0500

    update

--- 07 Local Storage by Building a Todo App/script.js
+++ 07 Local Storage by Building a Todo App/script.js
@@ -10,7 +10,7 @@ const titleInput = document.getElementById('title-input');
 const dateInput = document.getElementById('date-input');
 const descriptionInput = document.getElementById('description-input');
 
-const taskData = [];
+const taskData = JSON.parse(localStorage.getItem('data')) || [];
 let currentTask = {};
 
 const addOrUpdateTask = () => {
@@ -29,6 +29,7 @@ const addOrUpdateTask = () => {
     taskData[dataArrIndex] = taskObj;
   }
 
+  localStorage.setItem('data', JSON.stringify(taskData));
   updateTaskContainer();
   reset();
 };
@@ -56,6 +57,7 @@ const deleteTask = (buttonEl) => {
 
   buttonEl.parentElement.remove();
   taskData.splice(dataArrIndex, 1);
+  localStorage.setItem('data', JSON.stringify(taskData));
 };
 
 const editTask = (buttonEl) => {
@@ -82,6 +84,10 @@ const reset = () => {
   currentTask = {};
 };
 
+if (taskData.length) {
+  updateTaskContainer();
+}
+
 openTaskFormBtn.addEventListener('click', () =>
   taskForm.classList.toggle('hidden')
 );
@@ -100,22 +106,3 @@ closeTaskFormBtn.addEventListener('click', () => {
     reset();
   }
 });
-
-cancelBtn.addEventListener('click', () => confirmCloseDialog.close());
-
-discardBtn.addEventListener('click', () => {
-  confirmCloseDialog.close();
-  reset();
-});
-
-taskForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  addOrUpdateTask();
-});
-
-const myTaskArr = [
-  { task: 'Walk the Dog', date: '22-04-2022' },
-  { task: 'Read some books', date: '02-11-2023' },
-  { task: 'Watch football', date: '10-08-2021' },
-];
