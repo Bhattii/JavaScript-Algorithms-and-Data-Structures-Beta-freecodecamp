c840485f04c482e4a4863765558b6f33852ffa39
Muhammad Imran <imran5244513@gmail.com>
2024-02-24 21:18:26 +0500

    create checkUserInput function

--- 08 Recursion by Building a Decimal to Binary Convertor/script.js
+++ 08 Recursion by Building a Decimal to Binary Convertor/script.js
@@ -1,3 +1,9 @@
 const numberInput = document.getElementById('number-input');
 const convertBtn = document.getElementById('convert-btn');
 const result = document.getElementById('result');
+
+const checkUserInput = () => {
+  console.log(numberInput.value);
+};
+
+convertBtn.addEventListener('click', checkUserInput);
